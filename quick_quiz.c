3501b0f3a0f0305d6062208ae6b6bead24b93565
Jasleen74 <preetjee@gmail.com>
2025-10-24 19:54:09 +0530

    Quick Quiz: Create a string using double quotes and print its content using a loop

---
+++ quick_quiz.c
@@ -0,0 +1,12 @@
+/*Quick Quiz: Create a string using double quotes and print its content using a loop.*/
+#include<stdio.h>
+int main() {
+    char mystring[]= {'s', 'h' , 'a', 'm' , '\0'};
+
+    for (int i = 0 ; i <4 ; i ++){
+        printf("Character is %c\n", mystring[i]);
+    }
+     
+  
+ return 0;
+}
