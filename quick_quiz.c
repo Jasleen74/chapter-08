5a018edd1e31328070036c0af4e65e64caac0dbb
Jasleen74 <preetjee@gmail.com>
2025-10-24 20:01:53 +0530

    Create a string using double quotes and print its content using a loop

--- quick_quiz.c
+++ quick_quiz.c
@@ -1,7 +1,9 @@
-/*Quick Quiz: Create a string using single quotes and print its content using a loop.*/
+/*Quick Quiz: Create a string using double quotes and print its content using a loop.*/
 #include<stdio.h>
 int main() {
-    char mystring[]= {'s', 'h' , 'a', 'm' , '\0'};
+    //char mystring[]= {'s', 'h' , 'a', 'm' , '\0'};
+    // Or we can write this as 
+    char mystring[] = "sham";
 
     for (int i = 0 ; i <4 ; i ++){
         printf("Character is %c\n", mystring[i]);
