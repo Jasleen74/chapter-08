9d0c56eef34efbf952ea6a1882b60a263e92b13a
Jasleen74 <preetjee@gmail.com>
2025-10-24 22:14:15 +0530

    Gets and puts uses

---
+++ gets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+      char string[30];
+
+      gets(string);
+
+     // printf("Your name is : %s", string);
+       puts(string);     // it pust the cursor in the next lines which simply means a \n 
+ 
+       printf("Oo hi : %s", string);
+      
+
+  
+ return 0;
+}  
