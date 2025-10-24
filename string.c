7c6ae709935ba37ef101ff5c004a32fdf6ea32fc
Jasleen74 <preetjee@gmail.com>
2025-10-24 19:38:57 +0530

    Basics of strings

---
+++ string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+      char string [] = {'s', 'h' , 'a', 'm' , '\0'};
+
+      printf("%c\n", string[2]);
+
+
+      //ANOTHER SIMPLER SHORT-CUT WAY 
+
+         char mystring[]= "Ramesh";
+
+         printf("%s", mystring);   // %s is a specifier for strings
+
+  
+ return 0;
+}
